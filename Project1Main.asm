e7c4e0efa80ca085f74b4dfed06575c41a197ab8
Alex Zhou <cosr330@gmail.com>
2024-02-08 16:15:06 -0800

    Updated main asm with my lab 3 code

--- Project1Main.asm
+++ Project1Main.asm
@@ -1,6 +1,3 @@
-;/* PROJECT 1 */
-; This project uses an LED as voltage reference connected to pin 6 (P1.7/AIN0)
-
 $NOLIST
 $MODN76E003
 $LIST
@@ -20,79 +17,132 @@ $LIST
 ;                               -------
 ;
 
-CLK               EQU 16600000 ; Microcontroller system frequency in Hz
-BAUD              EQU 115200 ; Baud rate of UART in bps
-TIMER1_RELOAD     EQU (0x100-(CLK/(16*BAUD)))
-TIMER0_RELOAD_1MS EQU (0x10000-(CLK/1000))
-TIMER0_RATE       EQU 4096     ; 2048Hz squarewave (peak amplitude of CEM-1203 speaker)
-TIMER0_RELOAD 	  EQU ((65536-(CLK/TIMER0_RATE)))
-TIMER2_RATE   	  EQU 1000     ; 1000Hz, for a timer tick of 1ms
-TIMER2_RELOAD 	  EQU ((65536-(CLK/TIMER2_RATE)))
+CLK                 EQU 16600000 ; Microcontroller system frequency in Hz
+BAUD                EQU 115200 ; Baud rate of UART in bps
+TIMER1_RELOAD       EQU (0x100-(CLK/(16*BAUD)))
+TIMER0_RELOAD_1MS   EQU (0x10000-(CLK/1000))
 
-ORG 0x0000
-	ljmp main
-	
-ORG 0x000B
-	ljmp Timer0_ISR
-	
-ORG 0x002B
-	ljmp Timer2_ISR
-
+org 0000H
+   ljmp Main
 ;                     1234567890123456    <- This helps determine the location of the counter
-test_message:     db '**THERMO TEST **', 0
-value_message:    db 'V(pin 14)=      ', 0
-temp_message: 	  db 'Temp=           ', 0
+test_message:     db '****LOADING*****', 0
+value_message:    db 'TEMP:      ', 0
+cel_message:	  db 'CELCIUS  READING',0
+fah_message:      db 'FARENHET READING',0
+CSEG
 
-
-TOGGLE			  equ P
-cseg
-; /* PORT SETTINGS */
+; /* LCD HARDWIRING */
 LCD_RS equ P1.3
 LCD_E  equ P1.4
 LCD_D4 equ P0.0
 LCD_D5 equ P0.1
 LCD_D6 equ P0.2
 LCD_D7 equ P0.3
-OPAMP  equ P0.4
-
+UNIT   equ P1.5
 $NOLIST
 $include(LCD_4bit.inc) ; A library of LCD related functions and utility macros
 $LIST
 
-; These register definitions needed by 'math32.inc'
+; /* MATH.INC STUFFS */
 DSEG at 30H
-x:   		  ds 4
-y:   		  ds 4
-bcd: 		  ds 5
-VLED_ADC: 	  ds 2
-temp: 		  ds 2
-Count1ms:     ds 2 ; Used to determine when half second has passed
-BCD_counter:  ds 1 ; The BCD counter incrememted in the ISR and displayed in the main loop
-clkup:		  ds 1
-net_temp:	  ds 3 ; Temp Reading after Thermocouple subtract LM335
+x:   ds 4
+y:   ds 4
+z:   ds 1
+
+bcd: ds 5
+
+
+VLED_ADC: ds 2
+dtemp:  ds 2
+temp1: ds 1
 
 BSEG
 mf: dbit 1
-half_seconds_flag: dbit 1
+cel: dbit 1
 
 $NOLIST
 $include(math32.inc)
 $LIST
 
-;                     1234567890123456    <- This helps determine the location of the counter
-Initial_Message:  db 'Time: xx:xx:xx  ', 0
-test:			  db 'test', 0
 
 
-Init_All:
-	; Configure all the pins for biderectional I/O
-	mov	P3M1, #0x00
-	mov	P3M2, #0x00
-	mov	P1M1, #0x00
-	mov	P1M2, #0x00
-	mov	P0M1, #0x00
-	mov	P0M2, #0x00
-	
+; /* Configure the serial port and baud rate */
+InitSerialPort:
+	mov	P3M1,#0x00  ; Configure all the pins for biderectional I/O
+	mov	P3M2,#0x00
+	mov	P1M1,#0x00
+	mov	P1M2,#0x00
+	mov	P0M1,#0x00
+	mov	P0M2,#0x00
+    ; Since the reset button bounces, we need to wait a bit before
+    ; sending messages, otherwise we risk displaying gibberish!
+    ;mov R1, #200
+    ;mov R0, #104
+    ;djnz R0, $   ; 4 cycles->4*60.285ns*104=25us
+    ;djnz R1, $-4 ; 25us*200=5.0ms
+    mov R2, #5
+    lcall waitms
+    ; Now we can proceed with the configuration of the serial port
+	orl	CKCON, #0x10 ; CLK is the input for timer 1
+	orl	PCON, #0x80 ; Bit SMOD=1, double baud rate
+	mov	SCON, #0x52
+	anl	T3CON, #0b11011111
+	anl	TMOD, #0x0F ; Clear the configuration bits for timer 1
+	orl	TMOD, #0x20 ; Timer 1 Mode 2
+	mov	TH1, #TIMER1_RELOAD
+	setb TR1
+    ret
+
+; /* Send a character using the serial port */
+putchar:
+    jnb TI, putchar
+    clr TI
+    mov SBUF, a
+    ret
+
+
+; Send a constant-zero-terminated string using the serial port
+SendString:
+    clr A
+    movc A, @A+DPTR
+    jz SendStringDone
+    lcall putchar
+    inc DPTR
+    sjmp SendString
+SendStringDone:
+    ret
+
+SendBin:
+	clr A
+	mov a, x+0
+	lcall putchar
+
+	clr A
+	mov a, x+1
+	lcall putchar
+
+	clr A
+	mov a, x+2
+	lcall putchar
+
+	clr A
+	mov a, x+3
+	lcall putchar
+
+	clr A
+	mov a, z
+	lcall putchar
+	ret
+
+ASCII_CHAR: 
+	db '0123456789ABCDEF'
+
+Hello_World:
+    DB  'Hello, World!', '\r', '\n', 0
+New_Line:
+	DB '\r', '\n', 0
+
+Init_Rest:
 	orl	CKCON, #0x10 ; CLK is the input for timer 1
 	orl	PCON, #0x80 ; Bit SMOD=1, double baud rate
 	mov	SCON, #0x52
@@ -108,43 +158,23 @@ Init_All:
 	anl	TMOD,#0xF0 ; Clear the configuration bits for timer 0
 	orl	TMOD,#0x01 ; Timer 0 in Mode 1: 16-bit timer
 	
-	; Initialize the pins used by the ADC (P1.1, P1.7) as input.
-	orl	P1M1, #0b10000010
-	anl	P1M2, #0b01111101
+	; Initialize the pin used by the ADC (P1.1) as input.
+	orl	P1M1, #0b00000010
+	anl	P1M2, #0b11111101
 	
 	; Initialize and start the ADC:
 	anl ADCCON0, #0xF0
 	orl ADCCON0, #0x07 ; Select channel 7
 	; AINDIDS select if some pins are analog inputs or digital I/O:
 	mov AINDIDS, #0x00 ; Disable all analog inputs
-	orl AINDIDS, #0b10000001 ; Activate AIN0 and AIN7 analog inputs
+	orl AINDIDS, #0b10000000 ; P1.1 is analog input
 	orl ADCCON1, #0x01 ; Enable ADC
+
 	
-    ; Since the reset button bounces, we need to wait a bit before
-    ; sending messages, otherwise we risk displaying gibberish!
-    mov R1, #200
-    mov R0, #104
-    djnz R0, $   ; 4 cycles->4*60.285ns*104=25us
-    djnz R1, $-4 ; 25us*200=5.0ms
-    
-    ; Now we can proceed with the configuration of the serial port
-	orl	CKCON, #0x10 ; CLK is the input for timer 1
-	orl	PCON, #0x80 ; Bit SMOD=1, double baud rate
-	mov	SCON, #0x52
-	anl	T3CON, #0b11011111
-	anl	TMOD, #0x0F ; Clear the configuration bits for timer 1
-	orl	TMOD, #0x20 ; Timer 1 Mode 2
-	mov	TH1, #TIMER1_RELOAD
-	setb TR1
 	
-	Set_Cursor(1,1)
-	Send_Constant_String(#Initial_Message)
-	Set_Cursor(1,13)
-	Display_BCD(BCD_counter)
-	mov R2, #0x00
-    
 	ret
-	
+
+; /* 1ms DELAY FUNCTIONS */
 wait_1ms:
 	clr	TR0 ; Stop timer 0
 	clr	TF0 ; Clear overflow flag
@@ -153,32 +183,59 @@ wait_1ms:
 	setb TR0
 	jnb	TF0, $ ; Wait for overflow
 	ret
-
-; Wait the number of miliseconds in R2
 waitms:
 	lcall wait_1ms
 	djnz R2, waitms
 	ret
 
-; We can display a number any way we want.  In this case with
-; four decimal places.
-Display_formated_BCD:
-	Set_Cursor(2, 8)
-	Display_BCD(bcd+3)
-	;Send_BCD(bcd+3)
+Display_formated_BCD: ;4 dig 
+	Set_Cursor(1, 1)
+    Send_Constant_String(#cel_message)
+	Set_Cursor(2, 7)
 	Display_BCD(bcd+2)
-	Send_BCD(bcd+2)
-	Display_char(#'.')
+	Set_Cursor(2, 9)
 	Display_BCD(bcd+1)
-	Send_BCD(bcd+1)
-	Set_Cursor(2,15)
-	Display_char(#0xdf)
-	Set_Cursor(2,16)
-	Display_char(#0x43)
-	;Display_BCD(bcd+0)
-	;Send_BCD(bcd+0)
+	Set_Cursor(2, 10)
+	Display_BCD(bcd+1)
+	
+	Set_Cursor(2, 12)
+	Display_BCD(bcd+0)
+	Set_Cursor(2, 10)
+	Display_char(#'.')
+	Set_Cursor(2, 7)
+	Display_char(#0x20)
+	Set_Cursor(2, 15)
+	Display_char(#0xDF)
+	Set_Cursor(2, 16)
+	Display_char(#'C')
+
 	ret
 
+Display_formated_BCD_F: ;4 dig 
+	Set_Cursor(1, 1)
+    Send_Constant_String(#fah_message)
+	Set_Cursor(2, 7)
+	Display_BCD(bcd+2)
+	Set_Cursor(2, 9)
+	Display_BCD(bcd+1)
+	Set_Cursor(2, 10)
+	Display_BCD(bcd+1)
+	
+	Set_Cursor(2, 12)
+	Display_BCD(bcd+0)
+	Set_Cursor(2, 10)
+	Display_char(#'.')
+	Set_Cursor(2, 7)
+	;Display_char(#0x20)
+	Set_Cursor(2, 15)
+	Display_char(#0xDF)
+	Set_Cursor(2, 16)
+	Display_char(#'F')
+
+	ret
+
+
+; /* READ ADC */
 Read_ADC:
 	clr ADCF
 	setb ADCS ;  ADC start trigger signal
@@ -198,213 +255,116 @@ Read_ADC:
     orl a, R0
     mov R0, A
 	ret
-	
-; Send a character using the serial port
-putchar:
-    jnb TI, putchar
-    clr TI
-    mov SBUF, a
-    ret
 
-; Send a constant-zero-terminated string using the serial port
-SendString:
-    clr A
-    movc A, @A+DPTR
-    jz SendStringDone
-    lcall putchar
-    inc DPTR
-    sjmp SendString
-SendStringDone:
-    ret
+Main:
+    mov SP, #0x7F ; Set the stack pointer to the begining of idata
     
-Newline:
-    DB  '\r', '\n', 0
-;NumTest:
-;	DB '2.9783', '\r', '\n', 0
-
-
-;---------------------------------;
-;      IMPORTED TIMER CODE		  ;
-;---------------------------------;
-; Routine to initialize the ISR   ;
-; for timer 0                     ;
-;---------------------------------;
-Timer0_Init:
-	orl CKCON, #0b00001000 ; Input for timer 0 is sysclk/1
-	mov a, TMOD
-	anl a, #0xf0 ; 11110000 Clear the bits for timer 0
-	orl a, #0x01 ; 00000001 Configure timer 0 as 16-timer
-	mov TMOD, a
-	mov TH0, #high(TIMER0_RELOAD)
-	mov TL0, #low(TIMER0_RELOAD)
-	; Enable the timer and interrupts
-    setb ET0  ; Enable timer 0 interrupt
-    setb TR0  ; Start timer 0
-	ret
-
-;---------------------------------;
-; ISR for timer 0.  Set to execute;
-; every 1/4096Hz to generate a    ;
-; 2048 Hz wave at pin SOUND_OUT   ;
-;---------------------------------;
-Timer0_ISR:
-	;clr TF0  ; According to the data sheet this is done for us already.
-	; Timer 0 doesn't have 16-bit auto-reload, so
-	clr TR0
-	mov TH0, #high(TIMER0_RELOAD)
-	mov TL0, #low(TIMER0_RELOAD)
-	setb TR0
-	reti
-
-;---------------------------------;
-; Routine to initialize the ISR   ;
-; for timer 2                     ;
-;---------------------------------;
-Timer2_Init:
-	mov T2CON, #0 ; Stop timer/counter.  Autoreload mode.
-	mov TH2, #high(TIMER2_RELOAD)
-	mov TL2, #low(TIMER2_RELOAD)
-	; Set the reload value
-	orl T2MOD, #0x80 ; Enable timer 2 autoreload
-	mov RCMP2H, #high(TIMER2_RELOAD)
-	mov RCMP2L, #low(TIMER2_RELOAD)
-	; Init One millisecond interrupt counter.  It is a 16-bit variable made with two 8-bit parts
-	clr a
-	mov Count1ms+0, a
-	mov Count1ms+1, a
-	; Enable the timer and interrupts
-	orl EIE, #0x80 ; Enable timer 2 interrupt ET2=1
-    setb TR2  ; Enable timer 2
-	ret
-
-;---------------------------------;
-; ISR for timer 2                 ;
-;---------------------------------;
-Timer2_ISR:
-	clr TF2  ; Timer 2 doesn't clear TF2 automatically. Do it in the ISR.  It is bit addressable.
-	cpl P0.4 ; To check the interrupt rate with oscilloscope. It must be precisely a 1 ms pulse.
-	
-	; The two registers used in the ISR must be saved in the stack
-	push acc
-	push psw
-	
-	; Increment the 16-bit one mili second counter
-	inc Count1ms+0    ; Increment the low 8-bits first
-	mov a, Count1ms+0 ; If the low 8-bits overflow, then increment high 8-bits
-	jnz Inc_Done
-	inc Count1ms+1
-
-Inc_Done:
-	; Check if half second has passed
-	;mov a, Count1ms+0
-	;cjne a, #low(1000), Timer2_ISR_done ; Warning: this instruction changes the carry flag!
-	;mov a, Count1ms+1
-	;cjne a, #high(1000), Timer2_ISR_done
-	
-	; 500 milliseconds have passed.  Set a flag so the main program knows
-	setb half_seconds_flag ; Let the main program know half second had passed
-	clr a
-	mov Count1ms+0, a
-	mov Count1ms+1, a
-	; Increment the BCD counter
-	;mov a, BCD_counter
-	;add a, #0x01
-	;da a ; Decimal adjust instruction.  Check datasheet for more details!
-	;mov BCD_counter, a
-	
-Timer2_ISR_done:
-	pop psw
-	pop acc
-	reti
-	
-main:
-	mov sp, #0x7f
-	lcall Init_All
+    lcall InitSerialPort
+    lcall Init_Rest
     lcall LCD_4BIT
-    lcall Timer0_Init
-    lcall Timer2_Init
-    setb EA
-    mov BCD_counter, #0x00
-    mov clkup, #0x00
-    
+
     ; initial messages in LCD
 	Set_Cursor(1, 1)
-    Send_Constant_String(#Initial_Message)
+    Send_Constant_String(#test_message)
 	Set_Cursor(2, 1)
-    ;Send_Constant_String(#value_message)
-    Send_Constant_String(#temp_message)
-    
-Forever:
+    Send_Constant_String(#value_message)
+	setb cel
 
-	; Read the 2.08V LED voltage connected to AIN0 on pin 6
-	anl ADCCON0, #0xF0
+	mov z, #0b00000001
+
+Forever: ;avaliable: r2, r3
+
+button:
+	jb UNIT, button_skip ; if the 'CLEAR' button is not pressed skip
+	Wait_Milli_Seconds(#50)	; Debounce delay.  This macro is also in 'LCD_4bit.inc'
+	jb UNIT, button_skip  ; if the 'CLEAR' button is not pressed skip
+	jnb UNIT, $		; Wait for button release.  The '$' means: jump to same instruction.
+	cpl cel
+	jb cel, celyes
+	mov z, #0b00000010
+	sjmp button_skip
+celyes:
+	mov z, #0b00000001
+button_skip:
+
+
+	; /* CALIBRATE */ 
+	anl ADCCON0, #0xF0 ; Read the 2.08V LED voltage connected to AIN0 on pin 6
 	orl ADCCON0, #0x00 ; Select channel 0
-
-	lcall Read_ADC
-	; Save result for later use
-	mov VLED_ADC+0, R0
+    lcall Read_ADC
+    
+	mov VLED_ADC+0, R0 ; Save result for later use
 	mov VLED_ADC+1, R1
 
-	; Read the signal connected to AIN7
-	anl ADCCON0, #0xF0
+	anl ADCCON0, #0xF0 ; Read the signal connected to AIN7
 	orl ADCCON0, #0x07 ; Select channel 7
 	lcall Read_ADC
-    
-    ; Convert to voltage
-	mov x+0, R0
+
+	jb cel, Celcius
+	ljmp Fah
+
+Celcius: 
+	mov x+0, R0 			; x <- adc(ch) 
 	mov x+1, R1
-	; Pad other bits with zero
-	mov x+2, #0
+	mov x+2, #0 			; pad w/0
 	mov x+3, #0
-	Load_y(20600) ; The MEASURED LED voltage: 2.074V, with 4 decimal places
-	lcall mul32
-	; Retrive the ADC LED value
-	mov y+0, VLED_ADC+0
+	Load_y(207000) 			; y <- (x.xxx (vled) * 1000) * 100
+	lcall mul32				
+    mov y+0, VLED_ADC+0 	; y <- adc(led)
 	mov y+1, VLED_ADC+1
-	; Pad other bits with zero
-	mov y+2, #0
-	mov y+3, #0
-	lcall div32
-	
-	Load_y(100)
-	lcall mul32
-	Load_y(2730000)
-	lcall sub32
-	
-	mov a, clkup
-	cjne a, #0x00, skipclk
-	add a, #0x01
-	mov clkup, a
-	
-	mov a, BCD_counter
-	add a, #0x01
-	da a ; Decimal adjust instruction.  Check datasheet for more details!
-	mov BCD_counter, a
-	ljmp clkdown
+	mov y+2, #0 			
+	mov y+3, #0 
+	lcall div32				; x <- adc(ch) * vled * 100 / adc(led)
+	Load_y(273150)			; y <- (2.7315 * 1000) * 100
+	lcall sub32	
 
-skipclk:
-	clr a
-	mov clkup, a
-clkdown:
-	; Convert to BCD and display
-	lcall hex2bcd
+	lcall hex2bcd ; Convert to BCD and display
 	lcall Display_formated_BCD
-	Set_Cursor(1,13)
-	Display_BCD(BCD_Counter)
-	
-	; Wait 500 ms between conversions
-	mov R2, #250
-	lcall waitms
-	mov R2, #250
-	lcall waitms
-	
-	
-	
-	mov DPTR, #Newline
-	lcall SendString
-	
-	ljmp Forever
+    lcall bcd2hex ;hex number now stored in x
 
+	ljmp Export	 		
+
+Fah:
+	mov x+0, R0 			; x <- adc(ch) 
+	mov x+1, R1
+	mov x+2, #0 			; pad w/0
+	mov x+3, #0
+	Load_y(207000) 			; y <- (x.xxx (vled) * 1000) * 100
+	lcall mul32					
+    mov y+0, VLED_ADC+0 	; y <- adc(led)
+	mov y+1, VLED_ADC+1
+	mov y+2, #0 			
+	mov y+3, #0 
+	lcall div32				; x <- adc(ch) * vled * 100 / adc(led)
+	Load_y(273150)			; y <- (2.7315 * 1000) * 100
+	lcall sub32
+
+	Load_y(9)
+	lcall mul32
+	Load_y(5)
+	lcall div32	
+	Load_y(32000)
+	lcall add32
 	
+	
+	lcall hex2bcd ; Convert to BCD and display
+	lcall Display_formated_BCD_F
+    lcall bcd2hex ;hex number now stored in x
+
+	ljmp Export	
+
+Export:	
+	mov R2, #250 ; Wait 500 ms between conversions
+	lcall waitms
+	mov R2, #250
+	lcall waitms
+
+	;mov dptr, #x
+    lcall SendBin
+	;mov dptr, #New_Line
+	;lcall SendString
+    ljmp Forever
+
 END
-	
+
+
