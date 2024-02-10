dc564056b3c39f4cbe6a5a2a134d20e40d06a412
Alex Zhou <cosr330@gmail.com>
2024-02-10 12:19:34 -0800

    Update temp.asm

---
+++ temp.asm
@@ -0,0 +1,20 @@
+; /* READ ADC */
+Read_ADC:
+	clr ADCF
+	setb ADCS ;  ADC start trigger signal
+    jnb ADCF, $ ; Wait for conversion complete
+    
+    ; Read the ADC result and store in [R1, R0]
+    mov a, ADCRL
+    anl a, #0x0f
+    mov R0, a
+    mov a, ADCRH   
+    swap a
+    push acc
+    anl a, #0x0f
+    mov R1, a
+    pop acc
+    anl a, #0xf0
+    orl a, R0
+    mov R0, A
+	ret
