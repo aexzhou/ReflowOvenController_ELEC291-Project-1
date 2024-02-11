2e26c5d1c25f3481492b7d968b58d069ef69504e
Alex Zhou <cosr330@gmail.com>
2024-02-10 17:42:03 -0800

    Update button.asm

--- button.asm
+++ button.asm
@@ -1,11 +1,16 @@
 ; Start/Stop button, to do this, 
 ; make routine which displays "stopped" for a little bit
+; Buttons
+; 1, select stage
+; 2, select time/temp
+; 3, "shift" to select inc or dec
+; 4, the actual inc/dec button
+
 
 ; Buttons, from left to right:
 
 
 
 
-Display_Stopped:
 
 
