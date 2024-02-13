73136ce0a8272445c6bee6625dd3caccfc287f58
Alex Zhou <cosr330@gmail.com>
2024-02-13 12:04:34 -0800

    Create master_slave.asm

---
+++ master_slave.asm
@@ -0,0 +1,141 @@
+; Example Assembly Snippet for N76E003: Master Initiates Communication with Slave
+
+; Assume I2C has been initialized and a start condition has been generated
+
+; Sending Slave Address with Write Command
+MOV A, #EEPROM_SLA ; EEPROM_SLA should have the slave address and the R/W bit for write operation
+MOV I2DAT, A       ; Load the slave address into the I2C data register
+CLR SI             ; Clear the SI flag to initiate the address transmission
+JNB SI, $          ; Wait for the SI flag to be set, indicating the end of the operation
+
+; Check if Slave Acknowledged the Address
+MOV A, I2STAT      ; Load the status register into the accumulator
+CJNE A, #0x18, ERROR_HANDLER ; Compare it with the status code for ACK received after SLA+W transmitted
+                             ; Jump to error handler if not matched
+
+; Transmit Data Byte to Slave
+MOV A, #0x55       ; Example data byte to send
+MOV I2DAT, A       ; Load the data byte into the I2C data register
+CLR SI             ; Clear the SI flag to initiate the data transmission
+JNB SI, $          ; Wait for the SI flag to be set, indicating the end of the operation
+
+; Check if Slave Acknowledged the Data
+MOV A, I2STAT      ; Load the status register into the accumulator
+CJNE A, #0x28, ERROR_HANDLER ; Compare it with the status code for ACK received after data transmitted
+                             ; Jump to error handler if not matched
+
+; Generate Stop Condition
+SETB STO           ; Set the STO bit to generate a stop condition
+JNB STO, $         ; Wait for the hardware to clear STO, indicating the stop condition has been transmitted
+
+; Proceed with the rest of the program or loop back for more communication
+; ...
+
+ERROR_HANDLER:
+; Handle error condition here, e.g., by logging the error, resetting the I2C interface, or trying again
+; ...
+
+; Assembly code example for N76E003 I2C Master mode operation
+; Note: This is a simplified example and might need adjustments for actual use.
+
+ORG 0000H ; Start of program
+
+; Initialization
+MOV A, #13H            ; Load I2C clock rate value into Accumulator
+MOV I2CLK, A           ; Set I2C clock rate
+SETB I2CEN             ; Enable I2C module by setting the I2CEN bit in I2CON register
+
+; Start condition
+SETB STA               ; Generate start condition by setting the STA bit in I2CON
+JNB SI, $              ; Wait for SI to be set indicating operation complete
+CLR SI                 ; Clear SI flag to proceed
+
+; Send device address and write command
+MOV A, #EEPROM_SLA     ; Load EEPROM slave address with write command into Accumulator
+MOV I2DAT, A           ; Set slave address and R/W bit in I2DAT register
+CLR SI                 ; Clear SI flag to start transmission
+JNB SI, $              ; Wait for SI to be set indicating operation complete
+
+; Error handling and loop for data transmission would go here
+; ...
+
+; Stop condition
+SETB STO               ; Generate stop condition by setting the STO bit in I2CON
+JNB STO, $             ; Wait for hardware to clear STO indicating stop condition has been transmitted
+CLR SI                 ; Clear SI flag
+
+; Assembly snippet for initializing the I2C module and generating a start condition on N76E003
+
+ORG 0000H ; Start of program
+
+; I2C Initialization
+; Assuming SYSCLK is set up and running at your desired system clock frequency
+
+; Set I2C Clock Rate for 100kHz assuming a 16MHz system clock
+; Formula: I2C Clock Rate = SYSCLK / (4 * (I2CLK + 1))
+; To get a 100kHz I2C clock, I2CLK = (SYSCLK / (4 * 100kHz)) - 1
+; For SYSCLK = 16MHz, I2CLK = (16,000,000 / (4 * 100,000)) - 1 = 39
+MOV A, #39          ; Load the value 39 into Accumulator for 100kHz I2C clock rate
+MOV I2CLK, A        ; Set the I2C clock rate
+
+; Enable I2C Module
+SETB I2CEN          ; Set the I2CEN bit in I2CON register to enable I2C module
+
+; Generate Start Condition
+SETB STA            ; Set the STA bit in I2CON to generate a start condition
+JNB SI, $           ; Wait for the SI flag to be set, indicating the start condition has been transmitted
+CLR SI              ; Clear SI flag to proceed with the next operation
+
+; Now the I2C module is initialized, and a start condition has been generated.
+; The next steps would typically involve sending the slave address and the R/W bit,
+; followed by the data byte(s) and then generating a stop condition.
+
+; Assembly snippet for reading data from a slave device using I2C on N76E003
+
+; Send Slave Address with Read Bit
+MOV A, #((EEPROM_SLA<<1) | 0x01) ; EEPROM_SLA should be the slave address. Left shift and add read bit (1)
+MOV I2DAT, A                    ; Load the slave address with read bit into I2DAT register
+CLR SI                          ; Clear SI flag to initiate the address transmission
+JNB SI, $                       ; Wait for the SI flag to be set, indicating the end of the operation
+
+; Check if Slave Acknowledged the Address
+MOV A, I2STAT                   ; Load the status register into the accumulator
+CJNE A, #0x40, ERROR_HANDLER    ; Compare it with the status code for ACK received after SLA+R transmitted
+                                ; Jump to error handler if not matched
+
+; Set to Receive Data with ACK for continuous reading or NACK for a single byte
+SETB AA                         ; Set the AA bit for acknowledging the received data
+CLR SI                          ; Clear SI flag to initiate the receive operation
+JNB SI, $                       ; Wait for the SI flag to be set, indicating data reception
+
+; Read the received data from the I2DAT register
+MOV A, I2DAT                    ; Move the received data into the accumulator
+
+; Prepare to send NACK after last byte if it's a single byte read operation
+CLR AA                          ; Clear the AA bit to send NACK after receiving the next byte
+CLR SI                          ; Clear SI flag to complete the operation
+JNB SI, $                       ; Wait for SI flag, indicating the end of byte reception
+
+; Generate Stop Condition to end communication
+SETB STO                        ; Set the STO bit to generate a stop condition
+JNB STO, $                      ; Wait for the hardware to clear STO, indicating the stop condition has been transmitted
+
+; Error handling or proceed with further processing
+; ...
+
+ERROR_HANDLER:
+; Handle I2C error here, such as by resetting I2C and trying again or logging the error
+; ...
+
+END
+
+
+
+
+
+
+
+
+END
+
+
