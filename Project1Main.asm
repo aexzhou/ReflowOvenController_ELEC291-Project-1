312fe3fccfa07aa2572fb43afda4facea4ab5f76
JoeGraham [mou_ikkai] <joegrahamglt04@outlook.com>
2024-02-13 08:34:41 -0800

    lk

--- Project1Main.asm
+++ Project1Main.asm
@@ -99,6 +99,7 @@ value_message:    db 'TEMP:      ', 0
 cel_message:	  db 'CELCIUS  READING',0
 fah_message:      db 'FARENHET READING',0
 abort_message: 	  db 'ABORTABORTABORT ', 0
+state_message:	  db 'Current State:  ', 0
 
 $NOLIST
 $include(LCD_4bit.inc) ; A library of LCD related functions and utility macros
@@ -531,10 +532,14 @@ Export:							; Data export to python
 
 FSM1:
 	mov a, FSM1_state
+	Set_Cursor(1,1)
+	Send_Constant_String(#state_message)
 
 FSM1_state0:
 	cjne a, #0, FSM1_state1 ; if FSM1_state (currently stored in a) is not equal to zero (ie. state zero), go to state 1
 	mov pwm, #0
+	Set_Cursor(1,14)
+	Display_BCD(#0x00)
 	clr seconds_flag
 	; check for push button input
 	jb START_BUTTON, FSM1_state0_done
@@ -547,6 +552,8 @@ FSM1_state0_done:
 FSM1_state1:
 	cjne a, #1, FSM1_state2
 	mov pwm, #100
+	Set_Cursor(1,14)
+	Display_BCD(#0x01)
 	mov seconds, #0
 	mov a, #150
 	clr c
@@ -560,6 +567,8 @@ FSM1_state1_done:
 FSM1_state2:
 	cjne a, #2, FSM1_state3
 	mov pwm, #20
+	Set_Cursor(1,14)
+	Display_BCD(#0x02)
 	jnb seconds_flag, FSM_state2_funk
 	;mov a, #60
 	mov a, SoakTime
@@ -579,6 +588,8 @@ FSM1_state2_done:
 FSM1_state3:
 	cjne a, #3, FSM1_state4
 	mov pwm, #100
+	Set_Cursor(1,14)
+	Display_BCD(#0x03)
 	;mov a, #220
 	mov a, ReflowTemp
 	clr seconds_flag
@@ -593,6 +604,8 @@ FSM1_state3_done:
 FSM1_state4:
 	cjne a, #4, FSM1_state5
 	mov pwm, #20 
+	Set_Cursor(1,14)
+	Display_BCD(#0x04)
 	jnb seconds_flag, FSM1_state4_funk
 	;mov a, #45
 	mov a, ReflowTime
@@ -612,6 +625,8 @@ FSM1_state4_done:
 FSM1_state5:
 	cjne a, #5, FSM1_abort_state		; if the state is not in 0-5, then it must be 10 (aka the abort state)
 	mov pwm, #0
+	Set_Cursor(1,14)
+	Display_BCD(#0x04)
 	mov a, #60
 	clr c
 	subb a, tempc
