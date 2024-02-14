b2669b260203ac23ef41054e12e6ccfcddf73d75
JoeGraham [mou_ikkai] <joegrahamglt04@outlook.com>
2024-02-13 17:27:49 -0800

    ss

--- slave.asm
+++ slave.asm
@@ -2,6 +2,19 @@
 
 ORG 0000H ; Start of program
 
+DSEG at 30H
+data:               ds 1
+
+InitAll:
+	; /*** SERIAL PORT INITIALIZATION ***/
+	mov	P3M1,#0x00  			; Configure all the pins for biderectional I/O
+	mov	P3M2,#0x00
+	mov	P1M1,#0x00
+	mov	P1M2,#0x00
+	mov	P0M1,#0x00
+	mov	P0M2,#0x00
+    setb EA
+
 ; I2C Slave Initialization
 MOV A, #SLAVE_ADDR ; Load your I2C slave address into accumulator
 MOV I2ADDR, A      ; Set the I2C slave address register
@@ -11,6 +24,44 @@ SETB AA            ; Enable acknowledgment to respond to I2C master
 ; Main loop
 MAIN_LOOP:
 JNB SI, $          ; Wait for the SI flag to be set indicating an I2C event
+; (STA,STO,SI,AA) = (1,0,0,X) -> don't need to adjust any of these values? read only?
+; case (I2STAT)
+; 00H -> error
+; 08H -> START transmitted, clear STA, load SLA+W/R: I2DAT = SLA_ADDR1
+; 10H -> Repeated START transmitted, clear STA, I2DAT = SLA_ADDR2 (i guess this won't be used with only one slave)
+; 40H -> SLA+R transmitted, acknowledged: AA = 1
+; 48H -> SLA+R transmitted, not acknowledged, STO = 1, AA = 1
+; 50H -> Data recieved, acknowledged, store recieved data: datain = I2DAT. 
+; 58H -> Data recieved, not acknowledged, store recieved data: datain = I2DAT, STO = 1, AA = 1
+
+; From diagram 15.9 Flow and Status of Slave Reciever Node
+; case (I2STAT)
+; 60H -> SLA+W has been recieved and ACK has been transmitted. I2DAT = SLA+W (of slave)
+; 80H -> data byte has been recieved, ACK transmitted. I2DAT = data byte
+; 88H -> data byte recieved, ACK transmitted, I2DAT = data byte
+; A0H -> STOP or repeated START has been recieved
+; I2STAT should be 60 or 68 regardless
+
+
+recheck:
+    mov a, I2STAT
+case80h:
+    cjne a, 80H, case88h
+    mov data, I2DAT
+    ljmp done 
+case88h:
+    cjne a, 88H, casea0h
+    mov data, I2DAT
+    ljmp done
+casea0h:
+    cjne a, A0H, errors
+    ljmp done
+errors:
+    ljmp recheck
+
+done:
+    Set_Cursor(1,1)
+    
 
 ; Read the I2C status register to determine the cause of the interrupt
 MOV A, I2STAT
