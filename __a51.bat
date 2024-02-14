e8afc6d53a8416f14f3321b0940d93aad075107d
hrudairajesh <148665111+hrudairajesh@users.noreply.github.com>
2024-02-14 10:38:32 -0800

    gd

--- __a51.bat
+++ __a51.bat
@@ -1,5 +1,5 @@
 @echo off
 C:
-cd "\Users\JoeGr\OneDrive\Documents\GitHub\ELEC-291-Project1\"
-"C:\Users\JoeGr\Documents\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\JoeGr\OneDrive\Documents\GitHub\ELEC-291-Project1\Project1Main.asm"
-echo Crosside_Action Set_Hex_File C:\Users\JoeGr\OneDrive\Documents\GitHub\ELEC-291-Project1\Project1Main.HEX
+cd "\Users\hruda\Documents\GitHub\ELEC-291-Project1\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\hruda\Documents\GitHub\ELEC-291-Project1\LCD_Pushbuttons.asm"
+echo Crosside_Action Set_Hex_File C:\Users\hruda\Documents\GitHub\ELEC-291-Project1\LCD_Pushbuttons.HEX
