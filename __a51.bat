3e84de02052e8e4ffcdc5aa6b7ac20df3b0e8cf5
JoeGraham [mou_ikkai] <joegrahamglt04@outlook.com>
2024-02-14 18:57:24 -0800

    dfghjkl

--- __a51.bat
+++ __a51.bat
@@ -1,5 +1,5 @@
 @echo off
 C:
 cd "\Users\JoeGr\OneDrive\Documents\GitHub\ELEC-291-Project1\"
-"C:\Users\JoeGr\Documents\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\JoeGr\OneDrive\Documents\GitHub\ELEC-291-Project1\backup.asm"
-echo Crosside_Action Set_Hex_File C:\Users\JoeGr\OneDrive\Documents\GitHub\ELEC-291-Project1\backup.HEX
+"C:\Users\JoeGr\Documents\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\JoeGr\OneDrive\Documents\GitHub\ELEC-291-Project1\Project1Main.asm"
+echo Crosside_Action Set_Hex_File C:\Users\JoeGr\OneDrive\Documents\GitHub\ELEC-291-Project1\Project1Main.HEX
